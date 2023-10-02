94571774069a2246074d848cb99b09ba2036b965
Kurt Abraham <77301679+KurtAbraham@users.noreply.github.com>
2023-10-02 15:46:51 -0400

    Add files via upload

---
+++ ANOVA.R
@@ -0,0 +1,8 @@
+# Kurt Abraham | 10-02-23 | To perform ANOVA
+# Reading some dummy data
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+
+df <- read.csv(PATH)
+
+df
